63a5f512f8ed7db6b5c451c33072157ec3951c1a
ibrohimmath <ahmadjonovibrohim062@gmail.com>
2024-04-25 19:45:16 +0500

    commit second

--- main.js
+++ main.js
@@ -3,4 +3,8 @@ const app = document.querySelector(".app");
 const h1 = document.createElement("h1");
 h1.textContent = "Hi";
 
+const p = document.createElement("p");
+p.textContent = "Lorem ipsum dolor sit amet";
+
 app.appendChild(h1);
+app.appendChild(p);
